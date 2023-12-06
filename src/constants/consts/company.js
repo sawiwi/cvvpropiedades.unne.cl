f4e0a0431eb68c8dffd8af36633c0cf8c85e63f9
Fabian Salas <fabian.salas.astete@gmail.com>
2023-12-06 15:23:07 -0300

    first commit

---
+++ src/constants/consts/company.js
@@ -0,0 +1,15 @@
+export const company = {
+  statusId: 1,
+  companyId: 20102,
+  codigoUsuarioMaestro: 10075,
+};
+
+export const companyForm = {
+  id: 20102,
+};
+
+export const paginationTopLimit = {
+  limit: 9,
+  topLimit: 999999999,
+  limitPage: 1,
+};
